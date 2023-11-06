a0d5f6b68a0691434f2e0585fe652cf53cdd2650
Ashish473797 <ashish473797@gmail.com>
2023-11-06 10:19:30 +0530

    updated

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -10,15 +10,15 @@ let married = false                 // boolean
 let speed;                          // undefine
 let value = Symbol('hello')         // symbol
 let empty = null                    // null
-let date = new Date("2022-03-25");  // date object
 
 // Non primitive datatype are::
 
 let student = { }                   // object
 let studentArr = []                 // array object
 let studentAction =  function(){}   // function object
+let date = new Date("2022-03-25");  // date object
 
 
 console.table([typeof name, typeof age, typeof phoneNo, typeof married, typeof speed, typeof value, typeof empty, typeof student, typeof studentArr, typeof date, typeof studentAction])
 
-// Note:  primitive datatype take memory in Stack whereas Non-primitive datatype take memory in Head and referance  stored in Stack.
+// Note:  primitive datatype take memory in Stack whereas Non-primitive datatype take memory in Head and referance stored in Stack.
